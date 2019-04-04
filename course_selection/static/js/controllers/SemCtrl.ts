b64b9925e9ac1806bfe1d9ee826043fb3e8363b9
Reilly Bova <rbova@princeton.edu>
2019-04-04 15:28:37 -0400

    Backdated for updating

--- course_selection/static/js/controllers/SemCtrl.ts
+++ course_selection/static/js/controllers/SemCtrl.ts
@@ -12,8 +12,8 @@ class SemCtrl {
         ];
 
     // TODO: get this from the server
-    private static CURRENT_SEMESTER_TERM_CODE = 1192;
-    private static LAST_AVAILABLE_TERM_CODE = 1202;
+    private static CURRENT_SEMESTER_TERM_CODE = 1154;
+    private static LAST_AVAILABLE_TERM_CODE = 1162;
     private semesters;
 
     constructor(private $scope,
