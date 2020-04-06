c65382c8dfd348eb23a29e202daf8a6c84d28336
Emre Cakir <cakirmehmete@gmail.com>
2020-04-05 21:55:25 -0400

    Update scrape_parse.py

--- course_selection/scrape_parse.py
+++ course_selection/scrape_parse.py
@@ -103,6 +103,7 @@ def scrape_parse_semester(term_code):
         dep_courses = tree.getroot()
         remove_namespace(dep_courses, PTON_NAMESPACE)
         parsed_courses = []
+        print(dep_courses)
         for term in dep_courses:
             for subjects in term:
                 for subject in subjects:
