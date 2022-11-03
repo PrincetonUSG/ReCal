6f4e15af0f563289aeeeb9639891796e1c3e5aa2
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-11-03 13:40:41 -0400

    Update only most recent term

--- course_selection/scrape_all.py
+++ course_selection/scrape_all.py
@@ -5,17 +5,16 @@ from scrape_import import scrape_import_course, ScrapeCounter
 
 
 def get_all_courses():
-    term_codes = settings.ACTIVE_TERMS
-    for term_code in term_codes:
-        try:
-            print "Scraping for semester " + str(term_code)
-            courses = scrape_parse_semester(term_code)
-            # just a sanity check in case we ever modify scrape_parse
-            [validate_course(x) for x in courses]
-            print("validated")
-            scrapeCounter = ScrapeCounter()
-            [scrape_import_course(x, scrapeCounter) for x in courses]
-            print str(scrapeCounter)
-            print "----------------------------------"
-        except Exception as e:
-            print e
+    term_code = settings.ACTIVE_TERMS[-1]
+    try:
+        print "Scraping for semester " + str(term_code)
+        courses = scrape_parse_semester(term_code)
+        # just a sanity check in case we ever modify scrape_parse
+        [validate_course(x) for x in courses]
+        print("validated")
+        scrapeCounter = ScrapeCounter()
+        [scrape_import_course(x, scrapeCounter) for x in courses]
+        print str(scrapeCounter)
+        print "----------------------------------"
+    except Exception as e:
+        print e
